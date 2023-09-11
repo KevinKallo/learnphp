804fd67de7db5edc17772b6e9ccabc6971df93e5
KevinKallo <kevin.kallo@tptlive.ee>
2023-09-11 13:22:07 +0300

    Add easy server script

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-var_dump($_SERVER);
+//var_dump($_SERVER);
+switch($_SERVER['REQUEST_URI']){
+    case '/':
+        echo'Home Page';
+        break;
+    case '/about':
+        echo'About Page';
+        break;
+    default:
+        echo'404';
+}
